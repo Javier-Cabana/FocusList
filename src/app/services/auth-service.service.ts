664ecac82ce695741dba62c74538c35e77d50de0
Javier-Cabana <fcojcz@gmail.com>
2025-05-20 17:06:04 +0200

    Creadas interfaces de Jwt y Login, creado servicio de autenticación y creado interceptor para las cabeceras

---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,35 @@
+// src/app/services/auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginRequest } from '../model/i-LoginRequest';
+import { JwtResponse } from '../model/i-JwtResponse';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  login(creds: LoginRequest): Observable<JwtResponse> {
+    return this.http
+      .post<JwtResponse>(`${this.baseUrl}/autenticacion/login`, creds)
+      .pipe(
+        tap(res => {
+          localStorage.setItem('jwt_token', res.token);
+          localStorage.setItem('token_type', res.type);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('jwt_token');
+    localStorage.removeItem('token_type');
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('jwt_token');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideHttpClient, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { AuthInterceptor } from './app/interceptors/interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),  // registra HttpClientModule internamente
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,        // permite apilar varios interceptors
+    },
   ],
 });
