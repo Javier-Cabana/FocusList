ab076bde6d9b7c170e92d77660acec2f135a6b0f
Javier-Cabana <fcojcz@gmail.com>
2025-05-21 16:51:38 +0200

    Error en rutas arreglado y página de registro maquetada

--- src/app/Pages/registro/registro.page.ts
+++ src/app/Pages/registro/registro.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonCol, IonGrid, IonRow, IonInput, IonButton } from '@ionic/angular/standalone';
+import { ToastService } from 'src/app/services/toast-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonCol, IonGrid, IonRow, IonInput, IonButton]
 })
 export class RegistroPage implements OnInit {
+  username = '';
+  email = '';
+  password = '';
+  passwordConfirm='';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toast: ToastService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegistro(){
+
   }
 
 }
