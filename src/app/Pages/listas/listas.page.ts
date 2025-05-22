7fe34c3ba9fd5f27cca6618daff26c0b6b49c042
Javier-Cabana <fcojcz@gmail.com>
2025-05-22 16:25:46 +0200

    Trabajando en listas

--- src/app/Pages/listas/listas.page.ts
+++ src/app/Pages/listas/listas.page.ts
@@ -2,9 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar,
-  IonCol, IonGrid, IonRow, IonInput, IonButton } from '@ionic/angular/standalone';
+  IonCol, IonGrid, IonRow, IonInput, IonButton,IonIcon,
+  IonList, IonItem, IonItemSliding, IonLabel, IonReorderGroup,
+  IonReorder, IonItemOption, IonItemOptions, IonInfiniteScroll,
+  IonInfiniteScrollContent } from '@ionic/angular/standalone';
 import { ToastService } from 'src/app/services/toast-service.service';
 import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { personCircle } from 'ionicons/icons';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-listas',
@@ -12,20 +18,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./listas.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
-    IonCol, IonGrid, IonRow, IonInput, IonButton]
+    IonCol, IonGrid, IonRow, IonInput, IonButton, IonIcon, IonList, IonItem, IonItemSliding,
+    IonLabel, IonReorderGroup, IonReorder, IonItemOption, IonItemOptions, IonInfiniteScroll,
+    IonInfiniteScrollContent]
 })
+
 export class ListasPage implements OnInit {
+  listas: any[] = [];
+  page = 0;
+  size = 10;
+  username = '';
+  isReorderDisabled = false;
 
   constructor(
     private router: Router,
     private toast: ToastService,
-  ) { }
+    private listaService: ListaService,
+    private http: HttpClient
+  ) {
+    addIcons({personCircle});
+   }
 
   ngOnInit() {
+    this.loadListas();
+  }
+
+  loadListas() {
+    this.listaService.getListasByUsuario(this.username, this.page, this.size).subscribe((res) => {
+      this.listas.push(...res.content); // asumiendo paginación estilo Spring
+    });
   }
 
   goToPerfil() {
     this.router.navigate(['/perfil']);
   }
 
+  addLista(){
+
+  }
 }
