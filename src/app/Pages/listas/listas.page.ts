a89b6c5024fe602fd955c2a47424d5d0b16d1d85
Javier-Cabana <fcojcz@gmail.com>
2025-05-22 11:15:13 +0200

    Trabajando en Listas

--- src/app/Pages/listas/listas.page.ts
+++ src/app/Pages/listas/listas.page.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonCol, IonGrid, IonRow, IonInput, IonButton } from '@ionic/angular/standalone';
+import { ToastService } from 'src/app/services/toast-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listas',
   templateUrl: './listas.page.html',
   styleUrls: ['./listas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonCol, IonGrid, IonRow, IonInput, IonButton]
 })
 export class ListasPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toast: ToastService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  goToPerfil() {
+    this.router.navigate(['/perfil']);
   }
 
 }
